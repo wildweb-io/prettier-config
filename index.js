3647df8f46a42a5652adc444bdc4a47720260405
pnxdxt <paul.nodet@gmail.com>
2023-04-29 20:15:32 +0200

    chore: setup repo and basic config

---
+++ index.js
@@ -0,0 +1,10 @@
+module.exports = {
+	arrowParens: 'avoid',
+	bracketSpacing: false,
+	jsxSingleQuote: true,
+	singleQuote: true,
+	trailingComma: 'all',
+	useTabs: true,
+	proseWrap: 'always',
+	printWidth: 80,
+};
